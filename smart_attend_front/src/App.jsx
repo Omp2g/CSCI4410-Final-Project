0bd113404b470f08ae0c3dc68e47b22c90cb06c3
Omp2g <om.p53307@gmail.com>
2025-04-14 13:05:30 -0500

    Update App.jsx

--- smart_attend_front/src/App.jsx
+++ smart_attend_front/src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Register from './components/Register';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Login from './components/Login';
+import Register from './components/Register';
 import Dashboard from './components/Dashboard';
 import QRGenerator from './components/QRGenerator';
 import QRScanner from './components/QRScanner';
@@ -9,15 +10,18 @@ import QRScanner from './components/QRScanner';
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/qr-generator" element={<QRGenerator classId={1} />} />
-        <Route path="/qr-scanner" element={<QRScanner />} />
-      </Routes>
+      <div>
+        <h1>Smart Attend</h1>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/qr-generator" element={<QRGenerator classId={1} />} />
+          <Route path="/qr-scanner" element={<QRScanner />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
 
-export default App
+export default App;
